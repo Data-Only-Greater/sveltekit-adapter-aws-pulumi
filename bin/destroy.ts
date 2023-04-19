94bd3fc56b72bb5fb8a046d3b3bbb6f263a8ed09
Mathew Topper <damm_horse@yahoo.co.uk>
2023-04-19 11:58:17 +0100

    feat: allow stateless stack destroy
    This update adds new features to the destroy script to allow stack
    destruction without an existing build state and to make it more robust.
    
    Changes are:
    
    + Added a --default-projects flag to use the paths to the default
      pulumi projects when calling destroy
    + Added a -s flag to set the stack name explicitly
    + Added a retry loop in the event of a timeout error (such as might
      happen for lamba@edge)
    + Added a -f and --force flag which will run pulumi cancel before
      trying to start a destroy operation.

--- bin/destroy.ts
+++ bin/destroy.ts
@@ -2,17 +2,24 @@
 
 import * as path from 'path'
 import { realpathSync } from 'fs'
-import { pathToFileURL } from 'url'
-import { spawnSync } from 'child_process'
+import { fileURLToPath, pathToFileURL } from 'url'
+import { spawnSync, SpawnSyncReturns } from 'child_process'
 import { createRequire } from 'node:module'
 
 import parseArgs from 'minimist'
 
 import { AWSAdapterProps } from '../adapter'
 
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
+
 export async function main(args: string[]): Promise<void> {
-  let artifactPath = 'build'
+  let pulumiPaths: string[] | undefined
+  let stackName: string | undefined
+
   const argv = parseArgs(args.slice(2))
+
+  let artifactPath = 'build'
+
   if (argv._.length) {
     artifactPath = argv._[0]
   }
@@ -25,24 +32,81 @@ export async function main(args: string[]): Promise<void> {
 
   try {
     adapterProps = require(propsPath)
+    pulumiPaths = adapterProps.pulumiPaths
+    stackName = adapterProps.stackName
   } catch (error: any) {
-    if (error.message.includes('Cannot find module')) {
-      return
-    } else {
+    if (!error.message.includes('Cannot find module')) {
       throw error
     }
   }
 
-  for (const pulumiPath of adapterProps.pulumiPaths!) {
-    spawnSync(
-      'pulumi',
-      ['destroy', '-f', '-s', adapterProps.stackName!, '-y', '--refresh'],
-      {
+  if ('default-projects' in argv) {
+    const serverPath = path.resolve(__dirname, '..', 'stacks', 'server')
+    const mainPath = path.resolve(__dirname, '..', 'stacks', 'main')
+    pulumiPaths = [serverPath, mainPath]
+  }
+
+  if ('s' in argv) {
+    stackName = argv.s
+  }
+
+  let abort: boolean = false
+
+  if (pulumiPaths === undefined) {
+    console.log('Paths to pulumi projects could not be determined.')
+    abort = true
+  }
+
+  if (pulumiPaths === undefined) {
+    console.log('Stack name could not be determined')
+    abort = true
+  }
+
+  if (abort) {
+    console.log('Aborting')
+    return
+  }
+
+  const maxRetries: number = 3
+  let retries: number
+  let exitCode: number
+  let comRes: SpawnSyncReturns<Buffer>
+
+  for (const pulumiPath of pulumiPaths!) {
+    retries = 0
+    exitCode = 1
+
+    if ('f' in argv || 'force' in argv) {
+      spawnSync('pulumi', ['cancel', '-s', stackName!, '-y'], {
         cwd: pulumiPath,
         stdio: [process.stdin, process.stdout, process.stderr],
         env: process.env,
+      })
+    }
+
+    while (exitCode !== 0 && retries !== maxRetries) {
+      if (retries > 0) {
+        console.log(`Retry ${retries} of ${maxRetries}`)
       }
-    )
+
+      comRes = spawnSync(
+        'pulumi',
+        ['destroy', '-f', '-s', stackName!, '-y', '--refresh'],
+        {
+          cwd: pulumiPath,
+          stdio: [process.stdin, process.stdout, process.stderr],
+          env: process.env,
+        }
+      )
+
+      if (comRes.status === null) {
+        exitCode = 0
+      } else {
+        exitCode = comRes.status
+      }
+
+      retries += 1
+    }
   }
 }
 
